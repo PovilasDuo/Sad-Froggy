9af2c7bb98876945bfe2bdafd6c0eb88e4f51adf
silvijatub <silvija951@gmail.com>
2024-03-08 16:35:04 +0200

    menu changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,11 +7,48 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-      
+    public GameObject SettingsPanel;
+    public GameObject MainPanel;
+ 	public Slider gameVolumeSlider;
+
+    void Start()
+	{
+		gameVolumeSlider.value = AudioListener.volume;
+		gameVolumeSlider.onValueChanged.AddListener(delegate { GameVolumeChange(); });
+	}
+
     public void PlayGame()
     {
 		SceneManager.LoadScene("MainScene");
 	}
     
+        public void ShowSettingsPanel()
+	{
+     
+            SettingsPanel.SetActive(true);
+            MainPanel.SetActive(false);
+	}
 
+		public void ExitGame()
+	{
+        #if UNITY_EDITOR
+		    UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+	}
+
+		public void Back()
+	{
+		MainPanel.SetActive(true);
+		if (SettingsPanel.active)
+		{
+			SettingsPanel.SetActive(false);
+		}
+	}
+
+	public void GameVolumeChange()
+	{
+		AudioListener.volume = gameVolumeSlider.value;
+	}   
 }
